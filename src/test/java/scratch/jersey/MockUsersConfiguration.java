1bc0ce5dcb024a42a62275313fcb8c113520f874
karlbennett <where_1ts@yahoo.com>
2014-11-03 17:25:37 +1300

    Removed the unnecessary configured mock.

--- src/test/java/scratch/jersey/MockUsersConfiguration.java
+++ src/test/java/scratch/jersey/MockUsersConfiguration.java
@@ -1,20 +1,11 @@
 package scratch.jersey;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import scratch.user.Users;
-
-import static org.mockito.Mockito.mock;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class MockUsersConfiguration {
-
-    @Bean
-    public Users mockUsers() {
-        return mock(Users.class);
-    }
 }
